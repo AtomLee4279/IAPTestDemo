c11810259592dfde68f6a89026a5d5760c8fc5dc
李一贤 <573096385@qq.com>
2018-09-28 15:07:19 +0800

    Initial Commit

---
+++ IAPTestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IAPTestDemo
+//
+//  Created by 李一贤 on 2018/9/28.
+//  Copyright © 2018 李一贤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IAPTestDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  IAPTestDemo
+//
+//  Created by 李一贤 on 2018/9/28.
+//  Copyright © 2018 李一贤. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
